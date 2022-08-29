10639d1a2b9f94577c3c27cc0fb2528493b7045d
krishnabakshi1 <75680402+krishnabakshi1@users.noreply.github.com>
2022-08-29 01:12:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ Xdata = pd.read_pickle("FinalX.pkl")
 def main():
 	st.title('Afib or Normal ECG Classifier')
 	st.subheader('Perform these steps before uploading the test/csv file -')
-	if uploaded file ==None:
+	if uploaded_file ==None:
 		return st.error(X)
 	uploaded_file = st.file_uploader('Upload the text/csv file.')
 	 
